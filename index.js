5736556bd86eda8796a7dc42948e70126fc249c8
Siddharth Kannan <mail@siddharthkannan.in>
2019-06-02 18:30:51 +0900

    Add colored nodes and good labels

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 require("dotenv").config()
 const request = require("request");
 const _ = require("lodash");
+const fs = require('fs');
 
 const options = {
     method: 'GET',
-    url: 'https://app.launchdarkly.com/api/v2/flags/projKey',
+    url: 'https://app.launchdarkly.com/api/v2/flags/default',
     headers: {
         "Authorization": process.env.LD_API_KEY
     }
 };
+
+const transform_flag_name = _.snakeCase;
 
 request(options, function (error, response, body) {
     if (error) throw new Error(error);
@@ -21,17 +24,62 @@ request(options, function (error, response, body) {
 
     const full_mapping = flags.items.map(flag => {
         let key = flag.key
-        let is_on = flag.environments.production
+        let status = flag.environments.production.on
+        let off_variation = flag.environments.production.offVariation
 
         return flag.environments.production.prerequisites.map(prereq => ({
-            base: key,
-            base_status: status,
+            key: transform_flag_name(key),
+            status,
+            off_variation,
             depends_on: {
-                key: prereq.key,
+                key: transform_flag_name(prereq.key),
                 variation: prereq.variation
-            }
+            },
+            variations: flag.variations
         }))
     })
 
-    console.log(full_mapping)
-});
+    console.log("Full mapping length: " + full_mapping.length)
+    let filtered_mapping = full_mapping.filter(e => !_.isEmpty(e))
+    console.log("Filtered mapping length: " + filtered_mapping.length)
+
+    let flat_mapping = _.flatten(filtered_mapping)
+    console.log("Flat mapping length: " + flat_mapping.length)
+
+    console.log(flat_mapping.length)
+    console.log(flat_mapping[0])
+    console.log(flat_mapping[flat_mapping.length-1])
+
+    let dot_file = [ ]
+    dot_file.push("digraph prereqs {")
+
+    let label_lines = flat_mapping.map(flag => {
+        return [
+            flag.key,
+            ' [ shape=box, label="',
+            flag.key,
+            ';',
+            '", color=',
+            flag.status ? 'green' : 'red',
+            ' ];'
+        ].join('')
+    })
+    dot_file = _.concat(dot_file, label_lines)
+
+    let flag_lines = flat_mapping.map(flag_with_dependency => {
+        return [
+            flag_with_dependency.key,
+            ' -> ',
+            flag_with_dependency.depends_on.key,
+            ' [ label="',
+            'variation ' + flag_with_dependency.depends_on.variation,
+            '" ];'
+        ].join('')
+    })
+
+    dot_file = _.concat(dot_file, flag_lines)
+
+    dot_file.push("}")
+
+    fs.writeFileSync("test.dot", dot_file.join('\n'))
+})
